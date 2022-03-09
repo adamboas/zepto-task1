e897f958c4d37fad7e27abe3f197bbb51df5290a
Adam Boas <adam.boas@gmail.com>
2022-03-09 11:24:25 +1100

    Added all the other numbers, couldn't be bothered repeating all the specs. Need to deal with divide by zero more elegantly

--- calculator.rb
+++ calculator.rb
@@ -15,17 +15,57 @@ class Calculator
     def divide(a,b)
       a / b
     end
+
+    def calc(a, args = nil)
+      if args.nil? 
+        a 
+      else
+        raise Error('Invalid arguments') unless args.length == 2
+        Calculator.send(args[0], a, args[1])
+      end
+    end
   end
 end
 
 # Number funtions
 def one(args = nil)
-  if args.nil? 
-    1 
-  else
-    raise Error('Invalid arguments') unless args.length == 2
-    Calculator.send(args[0], 1, args[1])
-  end
+  Calculator.calc(1, args)
+end
+
+def two(args = nil)
+  Calculator.calc(2, args)
+end
+
+def three(args = nil)
+  Calculator.calc(3, args)
+end
+
+def four(args = nil)
+  Calculator.calc(4, args)
+end
+
+def five(args = nil)
+  Calculator.calc(5, args)
+end
+
+def six(args = nil)
+  Calculator.calc(6, args)
+end
+
+def seven(args = nil)
+  Calculator.calc(7, args)
+end
+
+def eight(args = nil)
+  Calculator.calc(8, args)
+end
+
+def nine(args = nil)
+  Calculator.calc(9, args)
+end
+
+def zero(args = nil)
+  Calculator.calc(0, args)
 end
 
 
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -40,4 +40,44 @@ RSpec.describe "base functions", :type => :request do
       end
     end
   end
+
+  describe "two" do
+    describe "when no argument provided" do
+      it "returns the integer 1" do
+        expect(two).to   eq(2)
+      end
+    end
+
+    describe "when it has an add function as an argument" do
+      it "executes the calculation" do
+        expect(two(plus(1))).to eq(3)
+        expect(two(plus(2))).to eq(4)
+        expect(two(plus(3))).to eq(5)
+      end
+    end
+
+    describe "when it has an minus function as an argument" do
+      it "executes the calculation" do
+        expect(two(minus(1))).to eq(1)
+        expect(two(minus(2))).to eq(0)
+        expect(two(minus(3))).to eq(-1)
+      end
+    end
+
+    describe "when it has a times function as an argument" do
+      it "executes the calculation" do
+        expect(two(times(1))).to eq(2)
+        expect(two(times(2))).to eq(4)
+        expect(two(times(3))).to eq(6)
+      end
+    end
+
+    describe "when it has a divide_by function as an argument" do
+      it "executes the calculation" do
+        expect(two(divided_by(1))).to eq(2)
+        expect(two(divided_by(2))).to eq(1)
+        expect(two(divided_by(3))).to eq(0)
+      end
+    end
+  end
 end
